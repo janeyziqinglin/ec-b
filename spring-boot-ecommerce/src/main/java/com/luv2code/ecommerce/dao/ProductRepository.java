581f41a8055cf6b215b2a78a8cb2a04dd0e025bc
Janeyziqinglin <janeyziqinglin@gmail.com>
2023-05-19 09:16:03 -0400

    update cors.

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,7 @@ import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200")
+// @CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://ec-f.vercel.app")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
